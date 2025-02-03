011ed217e7d7b0bd6d2521b05850e951c1f0dd5c
ChusVazquez <chus@elpuig.xeill.net>
2025-02-03 20:50:41 +0100

    Añadir create en EstudioController

--- src/main/java/com/chusdev/ems/backend/api/controllers/EstudioController.java
+++ src/main/java/com/chusdev/ems/backend/api/controllers/EstudioController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +39,13 @@ public class EstudioController {
         }
 
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody Estudio estudio){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(service.save(estudio));
     }
 
     @PutMapping("/{id}")
